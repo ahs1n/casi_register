cc407613d43dae7950aea79d63b52e55a9481f74
Muhammad Hussain <ahsanpervez74@gmail.com>
2021-01-20 16:03:21 +0500

    Add SaveDraft of Section 02 to 04

--- app/src/main/java/edu/aku/hassannaqvi/casi_register/ui/sections/Section03WSActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/casi_register/ui/sections/Section03WSActivity.java
@@ -6,9 +6,8 @@ import android.widget.Toast;
 
 import com.validatorcrawler.aliazaz.Validator;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -17,8 +16,6 @@ import edu.aku.hassannaqvi.casi_register.contracts.FormsContract;
 import edu.aku.hassannaqvi.casi_register.core.DatabaseHelper;
 import edu.aku.hassannaqvi.casi_register.core.MainApp;
 import edu.aku.hassannaqvi.casi_register.databinding.ActivitySection03WsBinding;
-import edu.aku.hassannaqvi.casi_register.models.Form;
-import edu.aku.hassannaqvi.casi_register.ui.other.MainActivity;
 import edu.aku.hassannaqvi.casi_register.utils.AppUtilsKt;
 
 import static edu.aku.hassannaqvi.casi_register.core.MainApp.form;
@@ -42,7 +39,11 @@ public class Section03WSActivity extends AppCompatActivity {
 
     public void BtnContinue() {
         if (!formValidation()) return;
-        SaveDraft();
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (UpdateDB()) {
             finish();
             startActivity(new Intent(this, Section01CSActivity.class));
@@ -67,33 +68,152 @@ public class Section03WSActivity extends AppCompatActivity {
     }
 
 
-    private void SaveDraft() {
+    private void SaveDraft() throws JSONException {
 
-        /*
-        form = new Form();
-        form.setSysdate(new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(new Date().getTime()));
-        form.setUsername(MainApp.userName);
-        form.setDeviceID(MainApp.appInfo.getDeviceID());
-        form.setDevicetagID(MainApp.appInfo.getTagName());
-        form.setAppversion(MainApp.appInfo.getAppVersion());
-        MainApp.setGPS(this);
-        */
+        JSONObject json = new JSONObject();
+        json.put("ws01", bi.ws01.getText().toString());
 
-        form = new Form();
-        form.setSysdate(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(new Date().getTime()));
-        form.setUsername(MainApp.userName);
-        form.setDeviceID(MainApp.appInfo.getDeviceID());
-        form.setDevicetagID(MainApp.appInfo.getTagName());
-        form.setAppversion(MainApp.appInfo.getAppVersion());
+        json.put("ws01a", bi.ws01a.getText().toString());
 
-        form.setCountry(MainActivity.mainInfo.getCountry());
-        form.setDistrict(MainActivity.mainInfo.getDistrict());
-        form.setUc(MainActivity.mainInfo.getUc());
-        form.setVillage(MainActivity.mainInfo.getVillage());
-        MainApp.setGPS(this);
+        json.put("ws01b", bi.ws01b.getText().toString());
 
+        json.put("ws02", bi.ws0201.isChecked() ? "1"
+                : bi.ws0202.isChecked() ? "2"
+                : bi.ws0203.isChecked() ? "3"
+                : "-1");
 
-        form.setsA(form.sAtoString());
+        json.put("ws03", bi.ws03.getText().toString());
+
+        json.put("ws04", bi.ws04.getText().toString());
+
+        json.put("ws05", bi.ws05.getText().toString());
+
+        json.put("ws05a", bi.ws05a.getText().toString());
+
+        json.put("ws06", bi.ws0601.isChecked() ? "1"
+                : bi.ws0602.isChecked() ? "2"
+                : bi.ws0603.isChecked() ? "3"
+                : bi.ws0604.isChecked() ? "4"
+                : bi.ws0605.isChecked() ? "5"
+                : bi.ws0696.isChecked() ? "96"
+                : "-1");
+
+        json.put("ws0696x", bi.ws0696x.getText().toString());
+        json.put("ws07", bi.ws07.getText().toString());
+
+        json.put("ws0801", bi.ws0801.getText().toString());
+
+        json.put("ws0802", bi.ws0802.getText().toString());
+
+        json.put("ws0803", bi.ws0803.getText().toString());
+
+        json.put("ws09", bi.ws09.getText().toString());
+
+        json.put("ws10", bi.ws10.getText().toString());
+
+        json.put("ws11", bi.ws11.getText().toString());
+
+        json.put("ws12", bi.ws12.getText().toString());
+
+        json.put("ws12a", bi.ws12a.getText().toString());
+
+        json.put("ws13", bi.ws13.getText().toString());
+
+        json.put("ws14", bi.ws1401.isChecked() ? "1"
+                : bi.ws1402.isChecked() ? "2"
+                : bi.ws1403.isChecked() ? "3"
+                : "-1");
+
+        json.put("ws15", bi.ws15.getText().toString());
+
+        json.put("ws16", bi.ws16.getText().toString());
+
+        json.put("ws17", bi.ws17.getText().toString());
+
+        json.put("ws18", bi.ws1801.isChecked() ? "1"
+                : bi.ws1802.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws19", bi.ws19.getText().toString());
+
+        json.put("ws2001", bi.ws2001.isChecked() ? "1" : "-1");
+
+        json.put("ws2002", bi.ws2002.isChecked() ? "2" : "-1");
+
+        json.put("ws2003", bi.ws2003.isChecked() ? "3" : "-1");
+
+        json.put("ws2004", bi.ws2004.isChecked() ? "4" : "-1");
+
+        json.put("ws2005", bi.ws2005.isChecked() ? "5" : "-1");
+
+        json.put("ws2006", bi.ws2006.isChecked() ? "6" : "-1");
+
+        json.put("ws21", bi.ws2101.isChecked() ? "1"
+                : bi.ws2102.isChecked() ? "2"
+                : bi.ws2103.isChecked() ? "3"
+                : bi.ws2104.isChecked() ? "4"
+                : "-1");
+
+        json.put("ws2201", bi.ws2201.isChecked() ? "1" : "-1");
+
+        json.put("ws2202", bi.ws2202.isChecked() ? "2" : "-1");
+
+        json.put("ws2203", bi.ws2203.isChecked() ? "3" : "-1");
+
+        json.put("ws2296", bi.ws2296.isChecked() ? "96" : "-1");
+
+        json.put("ws2296x", bi.ws2296x.getText().toString());
+        json.put("ws2301", bi.ws2301.getText().toString());
+
+        json.put("ws2302", bi.ws2302.getText().toString());
+
+        json.put("ws2303", bi.ws2303.getText().toString());
+
+        json.put("ws25a", bi.ws25a01.isChecked() ? "1"
+                : bi.ws25a02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25b", bi.ws25b01.isChecked() ? "1"
+                : bi.ws25b02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25c", bi.ws25c01.isChecked() ? "1"
+                : bi.ws25c02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25d", bi.ws25d01.isChecked() ? "1"
+                : bi.ws25d02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25e", bi.ws25e01.isChecked() ? "1"
+                : bi.ws25e02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25f", bi.ws25f01.isChecked() ? "1"
+                : bi.ws25f02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25g", bi.ws25g01.isChecked() ? "1"
+                : bi.ws25g02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25h", bi.ws25h01.isChecked() ? "1"
+                : bi.ws25h02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25i", bi.ws25i01.isChecked() ? "1"
+                : bi.ws25i02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25j", bi.ws25j01.isChecked() ? "1"
+                : bi.ws25j02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25k", bi.ws25k01.isChecked() ? "96"
+                : bi.ws25k02.isChecked() ? "2"
+                : "-1");
+
+        json.put("ws25k01x", bi.ws25k01x.getText().toString());
 
     }
 
--- app/src/main/java/edu/aku/hassannaqvi/casi_register/ui/sections/Section04WSFPActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/casi_register/ui/sections/Section04WSFPActivity.java
@@ -6,9 +6,8 @@ import android.widget.Toast;
 
 import com.validatorcrawler.aliazaz.Validator;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -17,8 +16,6 @@ import edu.aku.hassannaqvi.casi_register.contracts.FormsContract;
 import edu.aku.hassannaqvi.casi_register.core.DatabaseHelper;
 import edu.aku.hassannaqvi.casi_register.core.MainApp;
 import edu.aku.hassannaqvi.casi_register.databinding.ActivitySection04WsfpBinding;
-import edu.aku.hassannaqvi.casi_register.models.Form;
-import edu.aku.hassannaqvi.casi_register.ui.other.MainActivity;
 import edu.aku.hassannaqvi.casi_register.utils.AppUtilsKt;
 
 import static edu.aku.hassannaqvi.casi_register.core.MainApp.form;
@@ -42,7 +39,11 @@ public class Section04WSFPActivity extends AppCompatActivity {
 
     public void BtnContinue() {
         if (!formValidation()) return;
-        SaveDraft();
+        try {
+            SaveDraft();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         if (UpdateDB()) {
             finish();
             startActivity(new Intent(this, Section01CSActivity.class));
@@ -67,33 +68,175 @@ public class Section04WSFPActivity extends AppCompatActivity {
     }
 
 
-    private void SaveDraft() {
+    private void SaveDraft() throws JSONException {
 
-        /*
-        form = new Form();
-        form.setSysdate(new java.text.SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(new Date().getTime()));
-        form.setUsername(MainApp.userName);
-        form.setDeviceID(MainApp.appInfo.getDeviceID());
-        form.setDevicetagID(MainApp.appInfo.getTagName());
-        form.setAppversion(MainApp.appInfo.getAppVersion());
-        MainApp.setGPS(this);
-        */
+        JSONObject json = new JSONObject();
+        json.put("fw01", bi.fw01.getText().toString());
 
-        form = new Form();
-        form.setSysdate(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(new Date().getTime()));
-        form.setUsername(MainApp.userName);
-        form.setDeviceID(MainApp.appInfo.getDeviceID());
-        form.setDevicetagID(MainApp.appInfo.getTagName());
-        form.setAppversion(MainApp.appInfo.getAppVersion());
+        json.put("fw01a", bi.fw01a.getText().toString());
 
-        form.setCountry(MainActivity.mainInfo.getCountry());
-        form.setDistrict(MainActivity.mainInfo.getDistrict());
-        form.setUc(MainActivity.mainInfo.getUc());
-        form.setVillage(MainActivity.mainInfo.getVillage());
-        MainApp.setGPS(this);
+        json.put("fw01b", bi.fw01b.getText().toString());
 
+        json.put("fw02", bi.fw0201.isChecked() ? "1"
+                : bi.fw0202.isChecked() ? "2"
+                : bi.fw0203.isChecked() ? "3"
+                : "-1");
 
-        form.setsA(form.sAtoString());
+        json.put("fw03", bi.fw03.getText().toString());
+
+        json.put("fw04", bi.fw04.getText().toString());
+
+        json.put("fw05", bi.fw05.getText().toString());
+
+        json.put("fw05a", bi.fw05a.getText().toString());
+
+        json.put("fw06", bi.fw0601.isChecked() ? "1"
+                : bi.fw0602.isChecked() ? "2"
+                : bi.fw0603.isChecked() ? "3"
+                : bi.fw0604.isChecked() ? "4"
+                : bi.fw0605.isChecked() ? "5"
+                : bi.fw0696.isChecked() ? "96"
+                : "-1");
+
+        json.put("fw0696x", bi.fw0696x.getText().toString());
+        json.put("fw07", bi.fw07.getText().toString());
+
+        json.put("fw0801", bi.fw0801.getText().toString());
+
+        json.put("fw0802", bi.fw0802.getText().toString());
+
+        json.put("fw0803", bi.fw0803.getText().toString());
+
+        json.put("fw09", bi.fw09.getText().toString());
+
+        json.put("fw10", bi.fw10.getText().toString());
+
+        json.put("fw11", bi.fw11.getText().toString());
+
+        json.put("fw12", bi.fw12.getText().toString());
+
+        json.put("fw12a", bi.fw12a.getText().toString());
+
+        json.put("fw12b", bi.fw12b01.isChecked() ? "1"
+                : bi.fw12b02.isChecked() ? "2"
+                : bi.fw12b03.isChecked() ? "3"
+                : bi.fw12b04.isChecked() ? "4"
+                : bi.fw12b05.isChecked() ? "5"
+                : bi.fw12b96.isChecked() ? "96"
+                : "-1");
+
+        json.put("fw12b96x", bi.fw12b96x.getText().toString());
+        json.put("fw13", bi.fw13.getText().toString());
+
+        json.put("fw14", bi.fw1401.isChecked() ? "1"
+                : bi.fw1402.isChecked() ? "2"
+                : bi.fw1403.isChecked() ? "3"
+                : "-1");
+
+        json.put("fw15", bi.fw15.getText().toString());
+
+        json.put("fw16", bi.fw16.getText().toString());
+
+        json.put("fw17", bi.fw17.getText().toString());
+
+        json.put("fw1801", bi.fw1801.isChecked() ? "1" : "-1");
+
+        json.put("fw1802", bi.fw1802.isChecked() ? "2" : "-1");
+
+        json.put("fw1803", bi.fw1803.isChecked() ? "3" : "-1");
+
+        json.put("fw1804", bi.fw1804.isChecked() ? "4" : "-1");
+
+        json.put("fw1805", bi.fw1805.isChecked() ? "5" : "-1");
+
+        json.put("fw1806", bi.fw1806.isChecked() ? "6" : "-1");
+
+        json.put("fw19", bi.fw1901.isChecked() ? "1"
+                : bi.fw1902.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw2001", bi.fw2001.getText().toString());
+
+        json.put("fw21", bi.fw2101.isChecked() ? "1"
+                : bi.fw2102.isChecked() ? "2"
+                : bi.fw2103.isChecked() ? "3"
+                : bi.fw2104.isChecked() ? "4"
+                : "-1");
+
+        json.put("fw22", bi.fw22.getText().toString());
+
+        json.put("fw2301", bi.fw2301.getText().toString());
+
+        json.put("fw24", bi.fw24.getText().toString());
+
+        json.put("fw25", bi.fw25.getText().toString());
+
+        json.put("fw26", bi.fw2601.isChecked() ? "1"
+                : bi.fw2602.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw27", bi.fw2701.isChecked() ? "1"
+                : bi.fw2702.isChecked() ? "2"
+                : bi.fw2703.isChecked() ? "3"
+                : bi.fw2704.isChecked() ? "4"
+                : "-1");
+
+        json.put("fw2801", bi.fw2801.isChecked() ? "1" : "-1");
+
+        json.put("fw2802", bi.fw2802.isChecked() ? "2" : "-1");
+
+        json.put("fw2803", bi.fw2803.isChecked() ? "3" : "-1");
+
+        json.put("fw2804", bi.fw2804.isChecked() ? "4" : "-1");
+
+        json.put("fw2896", bi.fw2896.isChecked() ? "96" : "-1");
+
+        json.put("fw2896x", bi.fw2896x.getText().toString());
+        json.put("fw29a", bi.fw29a01.isChecked() ? "1"
+                : bi.fw29a02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29b", bi.fw29b01.isChecked() ? "1"
+                : bi.fw29b02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29c", bi.fw29c01.isChecked() ? "1"
+                : bi.fw29c02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29d", bi.fw29d01.isChecked() ? "1"
+                : bi.fw29d02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29e", bi.fw29e01.isChecked() ? "1"
+                : bi.fw29e02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29f", bi.fw29f01.isChecked() ? "1"
+                : bi.fw29f02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29g", bi.fw29g01.isChecked() ? "1"
+                : bi.fw29g02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29h", bi.fw29h01.isChecked() ? "1"
+                : bi.fw29h02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29i", bi.fw29i01.isChecked() ? "1"
+                : bi.fw29i02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29j", bi.fw29j01.isChecked() ? "1"
+                : bi.fw29j02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29k", bi.fw29k96.isChecked() ? "96"
+                : bi.fw29k02.isChecked() ? "2"
+                : "-1");
+
+        json.put("fw29k96x", bi.fw29k96x.getText().toString());
 
     }
 
